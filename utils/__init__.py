9378c4a3c69b4f558ab215aedb5108f71dbd5979
unknown <yumekinha@gmail.com>
2025-04-01 15:45:19 -0400

    Movido pastas __pycache__, services, ui e utils para dentro de web-source

--- utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# Este arquivo torna o diretório um pacote Python
-from .config import load_environment, get_app_settings
-
-__all__ = ['load_environment', 'get_app_settings'] 
--- web-source/__init__.py
+++ web-source/__init__.py
@@ -1,1 +1,4 @@
-# Root package initialization 
+# Este arquivo torna o diretório um pacote Python
+from .config import load_environment, get_app_settings
+
+__all__ = ['load_environment', 'get_app_settings'] 
